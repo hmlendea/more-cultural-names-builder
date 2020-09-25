c9b7646dd6e935c808aca510706ec0b45dbee536
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-25 19:38:11 +0300

    Vanilla CK2 configurations

--- Configuration/OutputSettings.cs
+++ Configuration/OutputSettings.cs
@@ -3,21 +3,19 @@ namespace MoreCulturalNamesModBuilder.Configuration
     public sealed class OutputSettings
     {
         public string ModOutputDirectory { get; set; }
+        public string ModVersion { get; set; }
+
+        public string CK2ModId { get; set; }
+        public string CK2ModName { get; set; }
 
         public string CK2HipModId { get; set; }
-
         public string CK2HipModName { get; set; }
 
         public string CK3GameVersion { get; set; }
-
         public string CK3ModId { get; set; }
-
         public string CK3ModName { get; set; }
 
         public string ImperatorRomeModId { get; set; }
-
         public string ImperatorRomeModName { get; set; }
-
-        public string ModVersion { get; set; }
     }
 }
