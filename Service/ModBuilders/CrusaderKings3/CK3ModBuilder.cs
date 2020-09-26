6a51cc3324f2a02f318db4879808a01f2f0cae25
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-09-26 17:12:13 +0300

    New CK3 builder

--- Service/ModBuilders/CrusaderKings3/CK3ModBuilder.cs
+++ Service/ModBuilders/CrusaderKings3/CK3ModBuilder.cs
@@ -8,15 +8,14 @@ using NuciDAL.Repositories;
 
 using MoreCulturalNamesModBuilder.Configuration;
 using MoreCulturalNamesModBuilder.DataAccess.DataObjects;
-using MoreCulturalNamesModBuilder.Service.Mapping;
 using MoreCulturalNamesModBuilder.Service.Models;
 
 namespace MoreCulturalNamesModBuilder.Service.ModBuilders.CrusaderKings3
 {
     public sealed class CK3ModBuilder : ModBuilder, ICK3ModBuilder
     {
-        const string LandedTitlesFileName = "999_MoreCulturalNames.txt";
-        const string NewLine = "\r\n";
+        const string InputLandedTitlesFileName = "ck3_landed_titles.txt";
+        const string OutputLandedTitlesFileName = "999_MoreCulturalNames.txt";
 
         public override string Game => "CK3";
 
@@ -56,8 +55,6 @@ namespace MoreCulturalNamesModBuilder.Service.ModBuilders.CrusaderKings3
                 .OrderBy(x => x.Key)
                 .ToDictionary(x => x.Key, x => x.ToList());
 
-            IEnumerable<Location> locations = locationRepository.GetAll().ToServiceModels();
-            
             string content = BuildLandedTitlesFile();
             WriteLandedTitlesFile(content, landedTitlesDirectoryPath);
         }
@@ -76,50 +73,127 @@ namespace MoreCulturalNamesModBuilder.Service.ModBuilders.CrusaderKings3
 
         string BuildLandedTitlesFile()
         {
-            string content = File.ReadAllText(Path.Combine(ApplicationPaths.DataDirectory, "ck3_landed_titles.txt"));
+            string landedTitlesFile = ReadLandedTitlesFile();
+            landedTitlesFile = CleanLandedTitlesFile(landedTitlesFile);
 
-            IEnumerable<Location> locations = locationRepository.GetAll().ToServiceModels();
+            List<string> landedTitlesFileLines = landedTitlesFile.Split('\n').ToList();
+            landedTitlesFileLines.Add(string.Empty);
 
-            foreach (Location location in locations)
+            IEnumerable<GameId> gameLocationIds = locations.Values
+                .SelectMany(x => x.GameIds)
+                .Where(x => x.Game == Game)
+                .OrderBy(x => x.Id);
+
+            List<string> content = new List<string> { string.Empty };
+
+            for (int i = 0; i < landedTitlesFileLines.Count - 1; i++)
             {
-                foreach (GameId gameLocation in location.GameIds.Where(x => x.Game == Game))
+                string line = landedTitlesFileLines[i];
+                string previousLine = content.Last();
+                string nextLine = landedTitlesFileLines[i + 1];
+
+                content.Add(line);
+
+                if (previousLine.Contains("allow") ||
+                    previousLine.Contains("limit") ||
+                    previousLine.Contains("trigger") ||
+                    nextLine.Contains("has_holder"))
                 {
-                    content = AddLocalisationsToTitle(content, gameLocation.Id);
+                    continue;
+                }
+
+                string titleId = Regex.Match(line, "^\\s*([ekdcb]_[^ =]*)[^=]\\s*=\\s*\\{[^\\{\\}]*$").Groups[1].Value;
+
+                if (string.IsNullOrWhiteSpace(titleId))
+                {
+                    continue;
+                }
+
+                Console.WriteLine(titleId);
+
+                string titleLocalisationsContent = GetTitleLocalisationsContent(line, titleId);
+
+                if (!string.IsNullOrWhiteSpace(titleLocalisationsContent))
+                {
+                    content.Add(titleLocalisationsContent);
                 }
             }
-
-            return content;
+            
+            return string.Join(Environment.NewLine, content.Skip(1));
         }
 
-        string AddLocalisationsToTitle(string landedTitlesFile, string gameId)
+        string GetTitleLocalisationsContent(string line, string gameId)
         {
             IEnumerable<Localisation> localisations = localisationFetcher.GetGameLocationLocalisations(gameId, Game);
 
-            if (!localisations.Any())
+            if (localisations.Count() == 0)
             {
-                return landedTitlesFile;
+                return null;
             }
 
-            string indentation1 = Regex.Match(landedTitlesFile, "^([ \t]*)" + gameId + "[ \t]*=[ \t]*\\{.*$", RegexOptions.Multiline).Groups[1].Value;
-            string indentation2 = indentation1 + "\t";
-            string indentation3 = indentation2 + "\t";
-            string thisContent = string.Empty;
+            string indentation1 = Regex.Match(line, "^(\\s*)" + gameId + "\\s*=\\s*\\{.*$").Groups[1].Value + "    ";
+            string indentation2 = indentation1 + "    ";
+            List<string> lines = new List<string>();
 
-            thisContent += $"{indentation2}cultural_names = {{" + NewLine;
+            lines.Add($"{indentation1}cultural_names = {{");
 
-            foreach (Localisation localisation in localisations)
+            foreach (Localisation localisation in localisations.OrderBy(x => x.LanguageId))
             {
-                thisContent += $"{indentation3}{localisation.LanguageId} = \"{localisation.Name}\"" + NewLine;
+                lines.Add($"{indentation2}{localisation.LanguageId} = \"{localisation.Name}\"");
             }
 
-            thisContent += $"{indentation2}}}" + NewLine;
+            lines.Add($"{indentation1}}}");
 
-            return Regex.Replace(landedTitlesFile, "^([ \t]*" + gameId + "[ \t]*=[ \t]*\\{.*$)", "$1\n" + thisContent, RegexOptions.Multiline);
+            return string.Join(Environment.NewLine, lines);
+        }
+
+        string CleanLandedTitlesFile(string content)
+        {
+            IEnumerable<GameId> gameLanguageIds = languages.Values
+                .SelectMany(x => x.GameIds)
+                .Where(x => x.Game == Game)
+                .OrderBy(x => x.Id);
+
+            string culturesPattern = string.Join('|', gameLanguageIds.Select(x => x.Id));
+
+            string newContent = content;
+            newContent = Regex.Replace(newContent, "\\t", "    ", RegexOptions.Multiline); // Replace tabs
+            newContent = Regex.Replace(newContent, "\\s*=\\s*", " = ", RegexOptions.Multiline); // Standardise spacings aroung equals
+            newContent = Regex.Replace(newContent, "\\s*#[^\r\n]*", "", RegexOptions.Multiline); // Remove comments
+            newContent = Regex.Replace(newContent, "^\\s*\n", "", RegexOptions.Multiline); // Remove empty/whitespace lines
+            newContent = Regex.Replace(newContent, "\\s+$", "", RegexOptions.Multiline); // Remove trailing whitespaces
+            
+            newContent = Regex.Replace( // Remove cultural names
+                newContent,
+                "^\\s*(" + culturesPattern + ")\\s*=\\s*\"*[^\"\r\n]*\"*[^\r\n]*\r*\n",
+                "",
+                RegexOptions.Multiline);
+
+            newContent = Regex.Replace( // Break ={} into multiple lines
+                newContent,
+                "(^\\s*)([^\\s]*\\s*=\\s*\\{)\\s*\\}",
+                "$1$2\n$1}",
+                RegexOptions.Multiline);
+
+            newContent = Regex.Replace( // Remove empty cultural_names blocks
+                newContent,
+                "^\\s*cultural_names\\s*=\\s*{\\s*\r*\n\\s*}\\s*\r*\n",
+                "",
+                RegexOptions.Multiline);
+
+            newContent = newContent.Replace("\r", "");
+            
+            return newContent;
+        }
+
+        string ReadLandedTitlesFile()
+        {
+            return File.ReadAllText(Path.Combine(ApplicationPaths.DataDirectory, InputLandedTitlesFileName));
         }
 
         void WriteLandedTitlesFile(string content, string landedTitlesDirectoryPath)
         {
-            string filePath = Path.Combine(landedTitlesDirectoryPath, LandedTitlesFileName);
+            string filePath = Path.Combine(landedTitlesDirectoryPath, OutputLandedTitlesFileName);
 
             File.WriteAllText(filePath, content);
         }
@@ -133,15 +207,16 @@ namespace MoreCulturalNamesModBuilder.Service.ModBuilders.CrusaderKings3
         string GenerateInnerDescriptorContent()
         {
             return
-                $"version=\"{outputSettings.ModVersion}\"" + NewLine +
-                $"tags={{" + NewLine +
-                $"    \"Culture\"" + NewLine +
-                $"    \"Historical\"" + NewLine +
-                $"    \"Map\"" + NewLine +
-                $"    \"Translation\"" + NewLine +
-                $"}}" + NewLine +
-                $"name=\"{outputSettings.CK3ModName}\"" + NewLine +
-                $"supported_version=\"{outputSettings.CK3GameVersion}\"";
+                $"# Version {outputSettings.ModVersion} ({DateTime.Now})" + Environment.NewLine +
+                $"name=\"{outputSettings.CK3ModName}\"" + Environment.NewLine +
+                $"version=\"{outputSettings.ModVersion}\"" + Environment.NewLine +
+                $"supported_version=\"{outputSettings.CK3GameVersion}\"" + Environment.NewLine +
+                $"tags={{" + Environment.NewLine +
+                $"    \"Culture\"" + Environment.NewLine +
+                $"    \"Historical\"" + Environment.NewLine +
+                $"    \"Map\"" + Environment.NewLine +
+                $"    \"Translation\"" + Environment.NewLine +
+                $"}}";
         }
     }
 }
