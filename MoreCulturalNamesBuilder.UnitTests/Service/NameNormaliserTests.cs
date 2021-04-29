9f74101b803cd03119fd8490dfe6f3b4ba9f37e7
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-04-30 00:32:41 +0300

    More name normalisation UTs

--- MoreCulturalNamesBuilder.UnitTests/Service/NameNormaliserTests.cs
+++ MoreCulturalNamesBuilder.UnitTests/Service/NameNormaliserTests.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using NUnit.Framework;
 
 using MoreCulturalNamesModBuilder.Service;
@@ -6,12 +8,32 @@ namespace MoreCulturalNamesModBuilder.UnitTests.Service
 {
     public class NameNormaliserTests
     {
+        const string Windows1252Characters = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+        
         private INameNormaliser nameNormaliser;
 
         [SetUp]
         public void SetUp()
         {
             this.nameNormaliser = new NameNormaliser();
+        }
+
+        [Test]
+        [TestCase("Basileia Rhṓmaiṓn", "Basileia Rhōmaiōn")]
+        [TestCase("Budapeşt", "Budapeşt")]
+        [TestCase("Đế quốc Nga", "Đê quôc Nga")]
+        [TestCase("Farƣona", "Farğona")]
+        [TestCase("Lò̤-mā Dá̤-guók", "Lò-mā Dá-guók")]
+        [TestCase("Lúksẹ́mbọ̀rg", "Lúksẹḿbọrg")]
+        [TestCase("Novyĭ Margelan", "Novyĭ Margelan")]
+        [TestCase("Starověký Řím", "Starověký Řím")]
+        public void GivenAName_WhenNormalisingForCK3_ReturnsTheExpectedNormalisedName(
+            string name,
+            string expectedResult)
+        {
+            string actualResult = nameNormaliser.ToCK3Charset(name);
+
+            Assert.AreEqual(expectedResult, actualResult);
         }
 
         [Test]
@@ -38,6 +60,26 @@ namespace MoreCulturalNamesModBuilder.UnitTests.Service
             string actualResult = nameNormaliser.ToWindows1252(name);
 
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        [TestCase("‎[]`{}´·ʹʺ–‘’”′∃̧̣̤̓́̀̆̂̌̈̄̍͘áÁàÀăĂắẮẵâÂấẤầẦǎåÅäÄǟãÃȧąāĀảẢạẠậẬæÆǣǢḃḂḅḄćĆĉĈčČċĊçÇďĎđĐḍḌḏḎðÐɖƉɗƊéÉèÈĕêÊếẾềỀểỂěĚëËẽẼėĖęĘēĒḗḖẻẹẸệỆǝƎəƏɛƐǵǴğĞĝĜǧǦġĠģĢɣƔƣƢĥĤḧḦḩḨħĦḥḤḫḪʻíÍìÌĭĬîÎǐǏïÏḯĩĨİįĮīĪịỊıɩʲĵĴǰǩǨķĶḳḲḵḴƙƘĺĹľĽļĻłŁḷḶɬḿḾṃṂⁿńŃǹǸňŇñÑṅṄņŅṇṆɲƝŋŊóÓòÒŏŎôÔốỐồỒổỔǒǑöÖȫȪőŐõÕȯøØǿǾǫǪōŌṓṒơƠờỜỡỠọỌộỘœŒɔṕṔŕŔřŘṛṚśŚŝŜšŠṡṠşŞṣṢșȘßťŤẗţŢṭṬțȚŧŦúÚùÙŭŬûÛǔǓůŮüÜǜǛűŰũŨųŲūŪủưƯứỨụỤṳṲʊƱṿṾẅẄẍẌýÝỳỲŷŶÿẏẎȳȲȝȜźŹžŽżŻẓẒƶƵʐþÞƿǷʼʾʿαάὰεΕέθΘιΙΟόύаАеЕіІјЈкКоОтТџЏ")]
+        public void GivenAName_WhenNormalisingForWindow1252_ReturnsTheNameWithoutCharactersOutsideCharset(
+            string name)
+        {
+            string actualResult = nameNormaliser.ToWindows1252(name);
+
+            string charsOutisdeCharset = string.Empty;
+
+            foreach (char c in actualResult)
+            {
+                if (!Windows1252Characters.Contains(c))
+                {
+                    charsOutisdeCharset += c;
+                }
+            }
+
+            Assert.AreEqual(string.Empty, charsOutisdeCharset);
         }
     }
 }
